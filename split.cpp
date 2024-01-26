79cbb989de7dfa874638f3bacf31a646ce41f9ff
Samuel Wu <samuelsw@usc.edu>
2024-01-25 23:31:13 -0800

    Committing split and updated gitignore

--- split.cpp
+++ split.cpp
@@ -11,13 +11,93 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
-
+#include <cstddef>
 /* Add a prototype for a helper function here if you need */
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+
+//When to terminate the recursive function
+if (in == nullptr) {
+  return;
+}
+
+//Making a new variable for the next value in linked list
+Node* nextVal = in->next;
+
+Node* evensVal = evens;
+Node* oddsVal = odds;
+
+if (in->value % 2 == 0) { //in node is even 
+
+  if (evens == nullptr){ //If the list is empty
+    evens = in;
+    evens->next = nullptr;
+
+    //Set what was just looked at to null
+    in = nullptr;
+    //Recursive call with updated in pointer
+    split(nextVal, odds, evens);
+  } else if (evensVal == nullptr) { //If evensVal is not the last item in the list
+    evensVal = in;
+    evensVal->next = nullptr;
+    
+    in = nullptr;
+    split(nextVal, odds, evens);
+    
+    //Navigating to proper spot in evens
+    //while (evensVal->next != nullptr){
+      //evensVal = evensVal->next;
+    //}
+    //split(in, odds, evens->next);
+
+    //Add in node to even list
+    //evensVal->next = in;
+    //Necessary?
+    //evensVal->next->next = nullptr;
+  } else {
+    split(in, odds, evensVal->next);
+  }
+  
+} else if (in->value % 2 == 1) { //Assume it can only be even or odd
+  
+  //If odds is empty
+  if (odds == nullptr){
+    odds = in;
+    odds->next = nullptr;
+    //Set what was just looked at to null
+    in = nullptr;
+    //Recursive call with updated in pointer
+    split(nextVal, odds, evens);
+  } else if (oddsVal == nullptr) { 
+    oddsVal = in;
+    oddsVal->next = nullptr;
+    
+    //Navigating to proper place in odds
+    //while (oddsVal->next != nullptr){
+      //oddsVal = oddsVal->next;
+    //}
+    //split(in, odds->next, evens);
+    //Odd
+    //oddsVal->next = in;
+    //oddsVal->next->next = nullptr;
+    in = nullptr;
+    split(nextVal, odds, evens);
+  } else {
+    split(in, oddsVal->next, evens);
+  }
+  
+  
+  
+}
+
+//Set what was just looked at to null
+//in = nullptr;
+//Recursive call with updated in pointer
+//split(nextVal, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,48 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
+    
+    Node* head = new Node(NULL, nullptr);
+    //head->next = new Node(2, nullptr);
+    //head->next->next = new Node(3, nullptr);
+    //head->next->next->next = new Node(4, nullptr);
+    //head->next->next->next->next = new Node(5, nullptr);
+
+    Node* evens = nullptr;
+    Node* odds = nullptr;
+
+    split(head, odds, evens);
+
+    Node* evenHead = evens;
+    while (evenHead != nullptr) {
+        std::cout << "Evens: " << evenHead->value << " " << std::endl;;
+        evenHead = evenHead->next;
+    }
+    std::cout << "evens ran" << std::endl;
+
+    Node* oddHead = odds;
+    while (oddHead != nullptr) {
+        std::cout << "Odds: " << oddHead->value << " " << std::endl;;
+        oddHead = oddHead->next;
+    }
+    std::cout << "odds ran" << std::endl;
+
+    
+    while (evens != nullptr) {
+        Node* tempe = evens;
+        evens = evens->next;
+        delete tempe;
+    }
+    while (odds != nullptr) {
+        Node* tempo = odds;
+        odds = odds->next;
+        delete tempo;
+    }
+
+    return 0;
 
 }
